4c36228ec18c6e8e482f5a5484e2dd844a2f026b
abdennuur <abdnourkhatib@gmail.com>
2024-07-04 18:21:09 +0000

    2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Task 2: basic annotations
+    - floor
+'''
+
+
+def floor(a: float) -> int:
+    '''To compute the floor of floating-point nbr.
+    '''
+    return int(a)
