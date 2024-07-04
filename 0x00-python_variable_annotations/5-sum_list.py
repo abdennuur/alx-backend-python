5f4bf0eac338cc1fd0388d3a4844e33212f9f7c0
abdennuur <abdnourkhatib@gmail.com>
2024-07-04 18:28:42 +0000

    5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Task 5.
+'''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''To compute the sum of list of floating-point nbrs.
+    '''
+    return float(sum(input_list))
